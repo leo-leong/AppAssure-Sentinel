92a75430690c3a32371eb8d17e808d97d1b7d596
Leo Leong <leoleo-31650@outlook.com>
2025-10-23 16:55:22 -0700

    Modified sample to use legacy HTTP Data Collector API

--- Sample2-IngestFunc/function_app.py
+++ Sample2-IngestFunc/function_app.py
@@ -1,15 +1,23 @@
-# information needed to send data to the DCR endpoint
-#TODO: Update DCE DCR and table details
-endpoint_uri = "https://<your-dce-name>.ingest.monitor.azure.com" # logs ingestion endpoint of the DCR
-dcr_immutableid = "dcr-00000000000000000000000000000000" # immutableId property of the Data Collection Rule
-stream_name = "Custom-<your-customtablename>" #name of the stream in the DCR that represents the destination table
+# Legacy code for uploading logs to Azure Monitor using HTTP Data Collector API
+# Refer step by step instructions, refer:
+#     https://learn.microsoft.com/en-us/previous-versions/azure/azure-monitor/logs/data-collector-api?tabs=python
+# Additional sample Function App Python code template:
+#     https://github.com/Azure/Azure-Sentinel/blob/master/DataConnectors/Templates/Connector_REST_API_AzureFunctionApp_template/Template_REST_API_AzureFunction_App_Code/Template_REST_API_Function_App_Python/Template_REST_API_Function_App_Python.py
+# Reference to the legacy HTTP Data Collector API 
+#     https://learn.microsoft.com/en-us/rest/api/loganalytics/create-request
+endpoint_uri = 'https://<Your-Workspace-ID>.ods.opinsights.azure.com/api/logs?api-version=2016-04-01' # this is HTTP endpoint for legacy Log Analytics workspace ingestion
+log_type = '<Your-Custom-Table-Name>'  # name of the custom table
+workspace_id = '<Your-Workspace-ID>' # Your Log Analytics workspace ID
+shared_key = '<Your-Shared-Key>' # Your Log Analytics workspace primary key
 
 import azure.functions as func
-from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
-from azure.monitor.ingestion import LogsIngestionClient
 from azure.core.exceptions import HttpResponseError
-import datetime
 import json
+import requests
+import hashlib
+import hmac
+import base64
+from datetime import datetime
 import logging
 
 app = func.FunctionApp()
@@ -23,8 +31,8 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('[TIMER] The timer is past due!')
 
-    # Sample data to send to the DCR
-    body = [
+    # Sample data to send to the Log Analytics
+    log_data = [
         {
             "Time": "2023-03-12T15:04:48.423211Z",
             "Computer": "Computer1",
@@ -49,17 +57,51 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
         }
     ]
 
-    try:
-        # authenticate and create client
-        credential = DefaultAzureCredential() # use this line to authenticate if the environment variable AZURE_CLIENT_ID is set
-        logging.info('[TIMER] DefaultAzureCredential initialized')
-        client = LogsIngestionClient(endpoint=endpoint_uri, credential=credential, logging_enable=True)
+    try:       
+        # Convert log data to JSON
+        body = json.dumps(log_data)
 
-        # upload data to the DCR
-        logging.info(f'[TIMER] Uploading {len(body)} records to DCR {dcr_immutableid} at {endpoint_uri}')
-        client.upload(rule_id=dcr_immutableid, stream_name=stream_name, logs=body)
+        # Post the data
+        post_data(workspace_id, shared_key, log_type, body)
     except HttpResponseError as e:
         logging.info(f"Upload failed: {e}")
 
+        
     timestamp = datetime.datetime.now()
     logging.info(f'[TIMER] Python timer trigger function end at {timestamp}')
+
+
+
+# **Build the signature**
+def build_signature(workspace_id, shared_key, date, content_length, method, content_type, resource):
+    x_headers = f'x-ms-date:{date}'
+    string_to_hash = f"{method}\n{content_length}\n{content_type}\n{x_headers}\n{resource}"
+    bytes_to_hash = bytes(string_to_hash, encoding="utf-8")
+    decoded_key = base64.b64decode(shared_key)
+    encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode()
+    return f"SharedKey {workspace_id}:{encoded_hash}"
+
+# **Send data to Azure Monitor**
+def post_data(workspace_id, shared_key, log_type, body):
+    method = 'POST'
+    content_type = 'application/json'
+    resource = f'/api/logs'
+    rfc1123date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    content_length = len(body)
+    signature = build_signature(workspace_id, shared_key, rfc1123date, content_length, method, content_type, resource)
+    uri = f'https://{workspace_id}.ods.opinsights.azure.com{resource}?api-version=2016-04-01'
+
+    headers = {
+        'Content-Type': content_type,
+        'Authorization': signature,
+        'Log-Type': log_type,
+        'x-ms-date': rfc1123date
+    }
+
+    response = requests.post(uri, data=body, headers=headers)
+    if response.status_code >= 200 and response.status_code <= 299:
+        logging.info('Data posted successfully!')
+    else:
+        logging.info(f'Failed to post data: {response.status_code}, {response.text}')
+
+
