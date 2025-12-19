52e6cd740b2027269e4315698746372050535b79
Leo Leong <leoleo-31650@outlook.com>
2025-12-18 16:23:00 -0800

    Initial commit for Sample4 CCF push connector and client-side agent

---
+++ Sample4-CCFPush-New/AppAssure-Sample4-CCFPush-New/ReleaseNotes.md
@@ -0,0 +1,4 @@
+| **Version** | **Date Modified (DD-MM-YYYY)** | **Change History**                          |
+|-------------|--------------------------------|---------------------------------------------|
+| 3.0.0       | 11-12-2025                     | Initial sample solution to demo use of CCF Push connector.
+
---
+++ Sample4-CCFPush-New/Sample-Agent-LogIngestionAPI/Sample-Agent-LogIngestionAPI/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Sample code for uploading custom logs to Azure Monitor using the Logs Ingestion API.
+ * Refer to the documentation for more details: https://learn.microsoft.com/en-us/azure/azure-monitor/logs/tutorial-logs-ingestion-code?tabs=net
+ */
+
+using Azure.Core;
+using Azure.Identity;
+using Azure.Monitor.Ingestion;
+
+// Initialize variables
+var endpoint = new Uri("https://my-url.monitor.azure.com");
+var ruleId = "dcr-00000000000000000000000000000000";
+var streamName = "Custom-MyTableRawData";
+
+// Create credential and client
+var credential = new DefaultAzureCredential();
+LogsIngestionClient client = new LogsIngestionClient(endpoint, credential);
+
+DateTime currentTime = DateTime.UtcNow;
+
+// Use BinaryData to serialize instances of an anonymous type into JSON
+BinaryData data = BinaryData.FromObjectAsJson(
+   new[] {
+    new
+    {
+       Time = currentTime,
+       Computer = "Computer1",
+       AdditionalContext = new
+       {
+         InstanceName = "user1",
+        TimeZone = "Pacific Time",
+        Level = 4,
+        CounterName = "AppMetric1",
+        CounterValue = 15.3
+       }
+    },
+    new
+    {
+       Time = currentTime,
+       Computer = "Computer2",
+       AdditionalContext = new
+       {
+        InstanceName = "user2",
+        TimeZone = "Central Time",
+        Level = 3,
+        CounterName = "AppMetric1",
+        CounterValue = 23.5
+       }
+    },
+   }
+);
+
+// Upload logs
+try
+{
+    //** ===== START: Use this block of code to upload uncompressed data.
+    var response = await client.UploadAsync(ruleId, streamName, RequestContent.Create(data)).ConfigureAwait(false);
+    if (response.IsError)
+    {
+        throw new Exception(response.ToString());
+    }
+    //** ===== End: code block to upload uncompressed data.
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Upload failed with Exception: " + ex.Message);
+}
